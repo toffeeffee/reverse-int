5c2254cdc53397b70c5efd0aca34ee3516ce3001
Irina Komarova <toffee@ukr.net>
2021-01-31 23:01:52 +0200

    init: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString();
+    let arr = [];
+    for (let i = 0; i < str.length; i++) {
+        arr[i] = (str[i]);
+    }
+    arr = arr.reverse();
+    str = '';
+    for (var i in arr) {
+        str = str + arr[i].toString();
+    }
+    n = parseInt(str);
+    return (n);
 }
